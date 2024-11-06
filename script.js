c1189e320ff5f91a4d7bbae260bcf8c187f86a01
LeoCRF <leomatheus22222@gmail.com>
2024-11-06 14:02:48 -0300

    Feat tarefa API

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-function calculadora(a, b, operacao) {
-    if (operacao === 'soma') {
-        return a + b;
-    } else if (operacao === 'subtracao') {
-        return a - b;
-    } else if (operacao === 'multiplicacao') {
-        return a * b;
-    } else if (operacao === 'divisao') {
-        if (b !== 0) {
-            return a / b;
-        } else {
-            return 'Divisão por zero não é permitida';
-        }
-    } else {
-        return 'Operação inválida';
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
 
-function calcular(operacao) {
-    const a = 10; // Número fixo
-    const b = 5;  // Número fixo
-    const resultado = calculadora(a, b, operacao);
-    console.log('Resultado:', resultado);
-}
+    const characterList = document.getElementById("character-list");
+
+
+    function fetchCharacters() {
+
+        const url = 'https://rickandmortyapi.com/api/character/?page=19';
+                fetch(url)
+            .then((response) => response.json()) 
+            .then((data) => {
+                displayCharacters(data.results); 
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar os personagens:", error); 
+            });
+    }
+
+
+    function displayCharacters(characters) {
+        characterList.innerHTML = ''; 
+
+
+        characters.forEach((character) => {
+
+            const characterCard = document.createElement("div");
+            characterCard.classList.add("character-card"); 
+
+            characterCard.innerHTML = `
+                <img src="${character.image}" alt="${character.name}">
+                <div class="card-body">
+                    <h2>${character.name}</h2>
+                    <p>Status: ${character.status}</p>
+                    <p>Espécie: ${character.species} ${character.type ? `- ${character.type}` : ''}</p>
+                    <p>Origem: ${character.origin.name}</p>
+                    <p>Localização: ${character.location.name}</p>
+                </div>
+            `;
+
+
+            characterList.appendChild(characterCard);
+        });
+    }
+
+  
+    fetchCharacters();
+});
